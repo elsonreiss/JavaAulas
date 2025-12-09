68cd01b246751630640a59d89e6d28f00978d841
elsonreis77 <elsonreis084@gmail.com>
2025-12-09 12:04:13 -0300

    commit aulasJava

--- src/oo/composicao/Compra.java
+++ src/oo/composicao/Compra.java
@@ -7,4 +7,19 @@ public class Compra {
     String nomeCliente;
     ArrayList<Item> itens = new ArrayList<Item>();
 
+    void adicionarItem(Item item) {
+        itens.add(item);
+        item.compra = this;
+    }
+
+    double valorTotal() {
+        double total = 0;
+
+        for (Item item : itens) {
+            total += item.quantidade * item.preco;
+        }
+
+        return total;
+    }
+
 }
--- src/oo/composicao/CompraTeste.java
+++ src/oo/composicao/CompraTeste.java
@@ -9,7 +9,11 @@ public class CompraTeste {
         c1.itens.add(new Item("Luva",1,100.00));
         c1.itens.add(new Item("Caneta",1,100.00));
 
-        System.out.println(c1.itens);
+        System.out.println(c1.itens.size());
+        System.out.println(c1.valorTotal());
+
+        double total = c1.itens.get(0).compra.itens.get(1).compra.valorTotal();
+        System.out.println(total);
 
     }
 }
