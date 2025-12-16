4bed98ef35f6a9e4685a8ac453c04582686fbdfc
elsonreis77 <elsonreis084@gmail.com>
2025-12-16 12:44:02 -0300

    commit aulasJava

--- src/oo/composicao/desafio/Cliente.java
+++ src/oo/composicao/desafio/Cliente.java
@@ -11,7 +11,33 @@ public class Cliente {
 
     Cliente(String nome, int idade) {
         this.nome = nome;
-        this.idade = idade;
+        this.setIdade(idade);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        if (idade > 0) {
+            this.idade = idade;
+        }
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+
+    public void setCompras(List<Compra> compras) {
+        this.compras = compras;
     }
 
     void adicionarCompra(Compra compra) {
